379093bc269d6e35e6b7643827c7523f32d8d735
zzj <15642532452@163.com>
2024-02-28 11:08:53 +0800

    zzj

--- src/tool/imageService.js
+++ src/tool/imageService.js
@@ -3,11 +3,11 @@ const images = ['bg0.jpg'];
 function getRandomBg() {
     const randomIndex = Math.floor(Math.random() * images.length);
     const imageName = images[randomIndex];
-    return 'public/img/bg/' + imageName;
+    return 'img/bg/' + imageName;
 }
 
 function getIcon(iconName) {
-    return 'public/img/icon/' + iconName;
+    return 'img/icon/' + iconName;
 }
 
 export { getRandomBg, getIcon };
