45ccae26b5fe9e43bf3d2e679fcd8b44e7b46565
zzj <15642532452@163.com>
2024-02-28 10:59:19 +0800

    zzj

--- src/tool/imageService.js
+++ src/tool/imageService.js
@@ -3,11 +3,11 @@ const images = ['bg0.jpg'];
 function getRandomBg() {
     const randomIndex = Math.floor(Math.random() * images.length);
     const imageName = images[randomIndex];
-    return 'https://accelerate-cdn.ccrui.cn/cc_personal_homepage/public/img/bg/' + imageName;
+    return '/public/img/bg/' + imageName;
 }
 
 function getIcon(iconName) {
-    return 'https://accelerate-cdn.ccrui.cn/cc_personal_homepage/public/img/icon/' + iconName;
+    return '/public/img/icon/' + iconName;
 }
 
-export {getRandomBg, getIcon};
+export { getRandomBg, getIcon };
