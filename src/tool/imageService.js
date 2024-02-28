5ba8381c945506dfa7f73ceefaf68210342a8675
zzj <15642532452@163.com>
2024-02-28 11:03:22 +0800

    zzj

--- src/tool/imageService.js
+++ src/tool/imageService.js
@@ -3,11 +3,11 @@ const images = ['bg0.jpg'];
 function getRandomBg() {
     const randomIndex = Math.floor(Math.random() * images.length);
     const imageName = images[randomIndex];
-    return '/public/img/bg/' + imageName;
+    return 'zzj_personal_homepage/public/img/bg/' + imageName;
 }
 
 function getIcon(iconName) {
-    return '/public/img/icon/' + iconName;
+    return 'zzj_personal_homepage/public/img/icon/' + iconName;
 }
 
 export { getRandomBg, getIcon };
